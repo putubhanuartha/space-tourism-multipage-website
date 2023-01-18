97996f324f99aab56bae3b72b23403c7d24710d0
Putu Gde Arya Bhanuartha <tuaryabhanu03@gmail.com>
2023-01-18 15:05:56 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Mainpage from './pages/Mainpage'
+
+
+function App() {
+
+
+  return (
+    <div id='App'>
+      <Mainpage/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import humbtn from "/assets/icon/icon-hamburger.svg";
+import closebtn from "/assets/icon/icon-close.svg";
+import "./Header.css";
+export default function Header() {
+  const [isSidebarActive, setIsSidebarActive] = useState(false);
+  return (
+    <div
+      id="header"
+      className="bg-transparent relative md:bg-black md:backdrop-blur-lg"
+    >
+      <div className="container mx-auto flex justify-between px-7 lg:px-20 items-center md:mt-6 mt-4">
+        <div id="logo">
+          <a href="#index.html">
+            <img
+              src="/assets/icon/logo.svg"
+              alt="logo"
+              className="w-9 h-9"
+            />
+          </a>
+        </div>
+        <div
+          id="topbar"
+          className="hidden md:block"
+        >
+          <ul className="flex">
+            <li>
+              <a href="">
+                <span>00</span> Home
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span>01</span> Destination
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span>02</span> Crew
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span>03</span> Technology
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div
+          id="sidebar"
+          className="bg-transparent absolute top-[9vh] right-0 rounded-lg backdrop-blur-lg md:hidden"
+        >
+          <ul>
+            <li>
+              <a href="">
+                <span>00</span> Home
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span>01</span> Destination
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span>02</span> Crew
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span>03</span> Technology
+              </a>
+            </li>
+          </ul>
+        </div>
+        <button
+          onClick={() => {
+            setIsSidebarActive(!isSidebarActive);
+          }}
+          id="hum-btn"
+          className="md:hidden"
+        >
+          <img
+            src={isSidebarActive ? closebtn : humbtn}
+            alt=""
+          />
+        </button>
+      </div>
+    </div>
+  );
+}
